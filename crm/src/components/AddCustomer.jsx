758f121c56c2c6c432ac4da337c753657ddd8da4
ThaaraniShivakumar <sivathaarani2004@gmail.com>
2025-06-07 13:24:23 +0530

    created a modal in addCustomer.js

---
+++ crm/src/components/AddCustomer.jsx
@@ -0,0 +1,23 @@
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+export const AddCustomer = () =>{
+    return(
+                <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }} >
+            <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Customer Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+            </Modal.Dialog>
+      </div> 
+    )
+}
--- crm/src/components/Customer.jsx
+++ crm/src/components/Customer.jsx
@@ -1,13 +1,16 @@
 import Button from 'react-bootstrap/Button';
+import { AddCustomer } from './AddCustomer';
 
 export const Customer =()=>{
     return (
         <div className='container '>
-            <div >
-<div className='d-flex d-column justify-content-end my-2'  >
+            <div className='d-flex flex-column gap-5' >
+<div className='d-flex justify-content-end my-2'  >
           <Button  variant="success">Add Customer</Button>
 </div>
-
+<div>
+    <AddCustomer/>
+</div>
             </div>
         </div>
     )
